0e0207cf31f837cffbbdac5cbcedade52de341b4
Jeremy Noh <jeremy@wertkt.com>
2018-11-16 16:02:51 +0100

    first player

--- server.js
+++ server.js
@@ -28,19 +28,71 @@ const start = async () => {
       mlog(`Server is running on port ${port}`);
     });
 
+    let users = []
+
     io.on("connection", socket => {
       mlog("client connected", "yellow");
 
       socket.on("disconnect", () => {
         mlog("client disconnected", "yellow");
+        nbPlay = nbPlay -1
       });
 
-      socket.on("join", nickname => {
-        console.log(`${nickname} has joined the channel`);
+      socket.on("newUser", nickname => {
+        users.push(nickname)
         socket.nickname = nickname;
-
-        socket.emit("hello", "Welcome to the jungle!");
+        console.log(users);
+        console.log(`${nickname} has joined the channel`);
       });
+
+      // magicUser
+        let nbPlay  = 0
+        let randomNumber = 0
+        let score = []
+        let nbRound = 0
+      socket.on("joint", () => {
+        console.log("le joueur ",socket.nickname,"");
+        nbPlay = nbPlay+1
+        // que tu peux pas tajouter deux fois au jeu
+        if (nbPlay == 2) {
+             randomNumber =  Math.floor((Math.random() * 1338) )
+             console.log(randomNumber);
+
+             socket.emit("start", "you can start the game")
+
+        }
+        else {
+          console.log("il y a : ",nbPlay);
+        }
+        // socket.emit("hello", "Welcome to the jungle!");
+      });
+
+      socket.on("try", number => {
+        console.log("randomNumber est : ", randomNumber);
+        console.log(" ton number est : ", number);
+      if (number == randomNumber  ) {
+        console.log(`The player : ${socket.nickname} wins the manche`);
+        io.emit("addPoint", `The player : ${socket.nickname} wins the manche`)
+        nbRound = nbRound+1
+        console.log(socket.nickname);
+        score.push(socket.nickname)
+      }
+      else if (number < randomNumber) {
+        console.log(socket.nickname);
+        console.log("your number is to lower");
+        socket.emit("tryAgain", "your number is to lower")
+
+      }
+      else if (number > randomNumber) {
+        console.log("your number is to bigger");
+        socket.emit("tryAgain", "your number is to bigger")
+
+      }
+      });
+
+
+      //finMagicUser
+
     });
   } catch (err) {
     mlog(err, "red");
